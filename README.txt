f94c1d6418d17eaf2ec0da31f90747694bffc8ff
martin <martin@localhost>
2016-09-21 11:13:01 +0100

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,80 @@
+.. image:: https://img.shields.io/codeship/8c6d7510-148d-0134-3d1b-7a5ab8a25fce/default.svg
+   :target: https://bitbucket.org/hellwig/cenvars-client
+.. image:: https://coveralls.io/repos/bitbucket/hellwig/cenvars-client/badge.svg?branch=default 
+   :target: https://coveralls.io/bitbucket/hellwig/cenvars-client?branch=default
+.. image:: https://img.shields.io/pypi/v/cenvars-client.svg
+   :target: https://pypi.python.org/pypi/Django-Integrator/
+.. image:: https://img.shields.io/badge/Donate-PayPal-blue.svg
+   :target: https://paypal.me/MartinHellwig
+.. image:: https://img.shields.io/badge/Donate-Patreon-orange.svg
+   :target: https://www.patreon.com/hellwig
+   
+
+##############
+Cenvars Client
+##############
+
+What is it?
+===========
+- The client library for cenvars.
+- The client script for cenvars.
+
+
+What problem does it solve?
+===========================
+XXX to be written
+
+How do I install it?
+====================
+pip install cenvars-client
+
+
+How do I use it?
+================
+XXX to be written
+
+Library
+-------
+XXX to be written
+
+.. sourcecode:: shell
+
+  >>> # pass
+  >>> # pass
+
+
+What license is this?
+=====================
+Two-clause BSD
+
+
+How can I get support?
+======================
+Please use the repo's bug tracker to leave behind any questions, feedback,
+suggestions and comments. I will handle them depending on my time and what looks
+interesting. If you require guaranteed support please contact me via
+e-mail so we can discuss appropriate compensation.
+
+
+Signing Off
+===========
+Is my work helpful or valuable to you? You can repay me by donating via:
+
+https://paypal.me/MartinHellwig
+
+.. image:: https://img.shields.io/badge/PayPal-MartinHellwig-blue.svg
+  :target: https://paypal.me/MartinHellwig
+  :alt: Donate via PayPal.Me
+  :scale: 120 %
+
+-or-
+
+https://www.patreon.com/hellwig
+
+.. image:: https://img.shields.io/badge/Patreon-hellwig-orange.svg
+  :target: https://www.patreon.com/hellwig
+  :alt: Donate via Patreon
+  :scale: 120 %
+
+
+Thank you!
---
+++ test.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2016, Martin P. Hellwig, All Rights Reserved.
+"""
+Unit Test module
+"""
+import unittest
+from rsa.pkcs1 import DecryptionError
+from cenvars import api, cli
+
+class TestAPI(unittest.TestCase):
+    "Generic Testing of the API"
+    def test_010_smoke(self, key_size=512):
+        "This should cover most code"
+        url = 'http://example.com/cenvars'
+        encoded_key = api.create_key(url, key_size,
+                                     print_function=lambda text: None)
+        r_url, key_size, identity, rsa_key = api.decode_key(encoded_key)
+        self.assertTrue(len(identity))
+        self.assertEqual(url, r_url)
+        test_data = {'some_key':'some_value'}
+        encrypted = api.encrypt(rsa_key, test_data)
+        decrypted = api.decrypt(rsa_key, encrypted, key_size)
+        self.assertEqual(test_data, decrypted)
+        return encoded_key, encrypted, test_data
+
+    def test_020_get(self):
+        "Testing the api.get"
+        encoded_key, encrypted, test_data = self.test_010_smoke()
+
+        import requests
+        mock_content = {'content':encrypted}
+
+        def mock(*args, **kwargs):
+            "Mocking requests.get"
+            mock_content['args'] = args
+            mock_content['kwargs'] = kwargs
+            return type('mock', (object,), mock_content)()
+
+        restore = requests.get
+        requests.get = mock
+
+        data = api.get(encoded_key, print_function=lambda text: None)
+        self.assertEqual(test_data, data)
+
+        # test content is damaged during transit
+        mock_content['content'] = encrypted[-5:] + encrypted[:-5]
+        self.assertRaises(DecryptionError, api.get, encoded_key)
+        mock_content['content'] = encrypted
+
+        # test environment_key is broken
+        broken_key = encoded_key[::-1]
+        self.assertRaises(api.constants.CenvarsEnvironmentError,
+                          api.get, broken_key)
+        requests.get = restore
+
+    def test_030_create_key_error(self):
+        "Does it raises an error when no url is specified?"
+        self.assertRaises(api.constants.CenvarsEnvironmentError,
+                          api.create_key, key_size=512)
+
+
+    def test_040_get_error(self):
+        "Does it raises an error when no key is specified?"
+        self.assertRaises(api.constants.CenvarsEnvironmentError, api.get)
+
+    def test_050_cli(self):
+        "Does it raise and appropiate error"
+        exception = api.constants.CenvarsEnvironmentError
+        self.assertRaises(exception, cli.cenvars)
+        self.assertRaises(exception, cli.cenvars_newkey)
+
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
